cd8fd872c478cf4e2e4f9140d6430a299355fcae
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-04 22:09:06 +0100

    Day 4 part 1

---
+++ day4/day4-1.js
@@ -0,0 +1,74 @@
+import { createInputFileReadLineInterface } from "../utils.js";
+
+const input = createInputFileReadLineInterface("./input.txt");
+
+let drawNumbers = []
+let boards = []
+let boardNumber = -1
+let boardRow = -1
+
+const getRow = (board, row) => {
+  return boards[board][row]
+}
+
+const getColumn = (board, column) => {
+  const selectedColumn = []
+  for (const index in boards[board]) {
+    const row = boards[board][index]
+    selectedColumn.push(row[column])
+  }
+  return selectedColumn
+}
+
+const bingoGame = () => {
+  for (let currentNumber = 5; currentNumber < drawNumbers.length; currentNumber++) {
+    const actualNumbers = drawNumbers.slice(0, currentNumber)
+    for (let indexBoard = 0; indexBoard < boards.length; indexBoard++) {
+      for (let indexRow = 0; indexRow < boards[indexBoard].length; indexRow++) {
+        const row = getRow(indexBoard, indexRow)
+        const column = getColumn(indexBoard, indexRow)
+        const winRow = row.every(numbers => actualNumbers.includes(numbers))
+        const winColumn = column.every(numbers => actualNumbers.includes(numbers))
+        
+        if(winRow || winColumn) {
+          console.log("board " + indexBoard + " acualnumber " +actualNumbers)
+          return result(boards[indexBoard], actualNumbers)
+        }
+      }
+    }
+  }
+}
+
+const result = (board, actualNumbers) => {
+  const lastNumber = parseInt(actualNumbers.slice(-1))
+  let sumNotSelectedNumbers = 0
+  for (let indexRow = 0; indexRow < board.length; indexRow++) {
+    const notSelected = board[indexRow].filter(number => {
+      return !actualNumbers.includes(number)
+    })
+    const sum = notSelected.reduce((acc, current) => acc + parseInt(current), 0)
+    sumNotSelectedNumbers += sum
+  }
+  console.log("suma ", sumNotSelectedNumbers)
+  return sumNotSelectedNumbers * lastNumber
+}
+
+input.on("line", (newLineFromInput) => {
+  if(drawNumbers.length === 0) {
+    drawNumbers = newLineFromInput.split(",")
+    return
+  }
+  if(newLineFromInput === "") {
+    boardNumber +=1
+    boardRow = -1
+    boards[boardNumber] = []
+    return
+  }
+  boardRow += 1
+  const row = newLineFromInput.split(' ').filter(number => number)
+  boards[boardNumber].push(row)
+});
+
+input.on("close", () => {
+  console.log(bingoGame())
+});
